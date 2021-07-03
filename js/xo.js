42244394e62c996f6f9f4076e9af537f9a0244a9
Vit <vitaliivassilenko@gmail.com>
2021-07-03 19:05:38 +0600

    all works fine

--- js/xo.js
+++ js/xo.js
@@ -1,40 +1,71 @@
 "use strict";
-const cell = document.querySelectorAll('.cell'),
+const cells = document.querySelectorAll('.cell'),
       button1 = document.getElementById('1_player'),
       button2 = document.getElementById('2_player'),
       button3 = document.getElementById('new_game');
 
-function Oplay () {
-    cell.forEach((item) =>{
-        item.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log(e.target);
-        item.lastElementChild.classList.add ("visible");
-        item.firstElementChild.classList.remove("visible");
-    });
-    });
+ 
+let field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    player = 1;
+
+    
+function playerClick () {
+    cells.forEach((cell, i) => {
+        cell.addEventListener('click', (e) => {
+        let y = i%3;
+        let x = (i-y)/3;
+        if (field[x][y] == 0) {
+            field[x][y]=player;
+            cell.classList.add(`player_${player}`);
+            if (checkWin()) {
+                alert(`Победил игрок ${player}`);
+            }
+            if (player == 1) {
+               player = 2; 
+            } else {
+                player = 1;
+            }
+        } 
+        console.log(field);
+        });
+    }
+    );
 }
 
-function Xplay () {
-    cell.forEach((item) =>{
-        item.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log(e.target);
-        item.firstElementChild.classList.add ("visible");
-        item.lastElementChild.classList.remove("visible");
-    });
-    });
+function checkWin () {
+    if (field[0][0]!== 0 && field[0][0] == field[0][1] && field[0][0] == field[0][2]) {
+        return true;
+    } else if (field[1][0]!== 0 && field[1][0] == field[1][1] && field[1][0] == field[1][2]) {
+        return true;
+    } else if (field[2][0]!== 0 && field[2][0] == field[2][1] && field[1][0] == field[2][2]) {
+        return true;
+    } else if (field[0][0]!== 0 && field[0][0] == field[1][0] && field[0][0] == field[2][0]) {
+        return true;
+    } else if (field[0][1]!== 0 && field[0][1] == field[1][1] && field[0][1] == field[2][1]) {
+        return true;
+    } else if (field[0][2]!== 0 && field[0][2] == field[1][2] && field[0][2] == field[2][2]) {
+        return true;
+    } else if (field[0][0]!== 0 && field[0][0] == field[1][1] && field[0][0] == field[2][2]) {
+        return true; 
+    } else if (field[0][2]!== 0 && field[0][2] == field[1][1] && field[0][2] == field[2][0]) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+
 
 function clearAll () {
-    cell.forEach((item) =>{
-        item.firstElementChild.classList.remove("visible");
-        item.lastElementChild.classList.remove("visible");
+    cells.forEach((cell) =>{
+        cell.classList.remove(`player_1`);
+        cell.classList.remove(`player_2`);
     });
+    field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    player = 1;
 }
 
 
 
-button1.addEventListener('click', Oplay);
-button2.addEventListener('click', Xplay);
+playerClick();
 button3.addEventListener('click', clearAll);
